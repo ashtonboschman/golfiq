31e8d50b90332b0231b6c29ecab3cf9b64eb6a7f
ashtonboschman <93393418+ashtonboschman@users.noreply.github.com>
2026-01-17 12:08:02 -0600

    Updates

--- lib/setup-ssl.ts
+++ lib/setup-ssl.ts
@@ -5,7 +5,5 @@ if (process.env.DB_CA_CERT) {
   const certPath = path.resolve('./vercel-db-ca.crt');
   const certData = Buffer.from(process.env.DB_CA_CERT, 'base64').toString('utf8');
   fs.writeFileSync(certPath, certData, { encoding: 'utf8' });
-
-  // Tell Node to use this certificate for SSL
   process.env.NODE_EXTRA_CA_CERTS = certPath;
 }
